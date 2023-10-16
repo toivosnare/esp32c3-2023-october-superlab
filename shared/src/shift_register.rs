2d9ca031eef2953cfb4aa3bc6208ea5852affe3c
Toivo Snåre <topisn@gmail.com>
2023-10-16 17:22:28 +0300

    Add missing shift register method implementations

--- shared/src/shift_register.rs
+++ shared/src/shift_register.rs
@@ -1,23 +1,41 @@
-#[derive(PartialEq)]
-pub struct ShiftRegister;
+#[derive(PartialEq, Default)]
+pub struct ShiftRegister {
+    data: [Option<u64>; 3],
+    write_index: usize,
+}
 
 impl ShiftRegister {
-    pub fn insert(&self, val: u64) {
-        todo!()
+    pub fn insert(&mut self, val: u64) {
+        self.data[self.write_index] = Some(val);
+        self.write_index += 1;
+        self.write_index %= 3;
     }
+
     pub fn avg(&self) -> u64 {
-        todo!()
+        let mut sum = 0u64;
+        let mut count = 0u64;
+        for &val in &self.data {
+            if let Some(num) = val {
+                sum += num;
+                count += 1;
+            }
+        }
+        if count == 0 {
+            0
+        } else {
+            sum / count
+        }
     }
 
     /// Don't use this. It's for testing :-)
     fn as_array(&self) -> &[Option<u64>] {
-        todo!()
+        &self.data
     }
 }
 
 #[test]
 fn insert_works() {
-    let mut sr = ShiftRegister;
+    let mut sr = ShiftRegister::default();
 
     sr.insert(1);
     assert_eq!(sr.as_array(), &[Some(1), None, None]);
@@ -32,8 +50,9 @@ fn insert_works() {
     assert_eq!(sr.as_array(), &[Some(4), Some(3), Some(2)]);
 }
 
+#[test]
 fn avg_works() {
-    let sr = ShiftRegister;
+    let mut sr = ShiftRegister::default();
 
     sr.insert(1);
     sr.insert(2);
